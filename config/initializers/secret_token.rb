bdb496a3dcdb47f86f6954eee25e9287a2d50ba9
Xa6i <thebasque@gmail.com>
2013-08-18 00:05:12 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PppApp::Application.config.secret_token = '75ff3b866de0d3b4869af7ada02b30dfc46643a276cde59e4a45f956f4db8c4d0b4b4b9ade4c4ec48a9c8ac35db1faaaa09fbc7b584813de57b8de21224da493'
